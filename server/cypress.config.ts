e00f168ff64fe14eeb9b83bb88691796762ae144
conversayShawn <shawn.harris3510@gmail.com>
2023-07-17 16:09:01 -0700

    testing v12 w/ conditional video

--- server/cypress.config.ts
+++ server/cypress.config.ts
@@ -17,18 +17,18 @@ export default defineConfig({
         createHero,
         deleteHero,
       });
-      // on("after:spec", (spec, results) => {
-      //   if (results && results.video) {
-      //     // Do we have failures for any retry attempts?
-      //     const failures = results.tests.some((test) =>
-      //       test.attempts.some((attempt) => attempt.state === "failed")
-      //     );
-      //     if (!failures) {
-      //       // delete the video if the spec passed and no tests retried
-      //       fs.unlinkSync(results.video);
-      //     }
-      //   }
-      // });
+      on("after:spec", (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === "failed")
+          );
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video);
+          }
+        }
+      });
     },
   },
 });
