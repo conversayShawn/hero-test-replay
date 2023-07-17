347a066fbde1bead4b79a4488f14eb7b784eb0fc
conversayShawn <shawn.harris3510@gmail.com>
2023-07-16 20:56:59 -0700

    add some console.logs and flaky/fail tests

--- server/cypress.config.ts
+++ server/cypress.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'cypress';
 import { createHero, deleteHero } from './cypress/support/data';
 
 export default defineConfig({
+  retries: {
+    runMode: 2
+  },
   projectId: 'yrin2w',
   e2e: {
     testIsolation: false, //disable to see api results screen
