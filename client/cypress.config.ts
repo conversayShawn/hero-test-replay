39f57cdcad5c9f9a4e4379d125cca9324c757137
conversayShawn <shawn.harris3510@gmail.com>
2023-07-17 19:53:55 -0700

    back to v13, removing conditional video

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "cypress";
 import { createHero, deleteHero } from './cypress/support/data';
-import fs from "fs";;
 
 export default defineConfig({
   retries: {
@@ -15,18 +14,6 @@ export default defineConfig({
         createHero,
         deleteHero,
       });
-      on("after:spec", (spec, results) => {
-        if (results && results.video) {
-          // Do we have failures for any retry attempts?
-          const failures = results.tests.some((test) =>
-            test.attempts.some((attempt) => attempt.state === "failed")
-          );
-          if (!failures) {
-            // delete the video if the spec passed and no tests retried
-            fs.unlinkSync(results.video);
-          }
-        }
-      });
     },
   },
 
@@ -34,20 +21,6 @@ export default defineConfig({
     devServer: {
       framework: 'react',
       bundler: 'vite',
-    },
-    setupNodeEvents(on, config) {
-      on("after:spec", (spec, results) => {
-        if (results && results.video) {
-          // Do we have failures for any retry attempts?
-          const failures = results.tests.some((test) =>
-            test.attempts.some((attempt) => attempt.state === "failed")
-          );
-          if (!failures) {
-            // delete the video if the spec passed and no tests retried
-            fs.unlinkSync(results.video);
-          }
-        }
-      });
     },
   },
 });
--- server/cypress.config.ts
+++ server/cypress.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'cypress';
 import { createHero, deleteHero } from './cypress/support/data';
-import fs from "fs";;
 
 export default defineConfig({
   retries: {
@@ -16,18 +15,6 @@ export default defineConfig({
       on('task', {
         createHero,
         deleteHero,
-      });
-      on("after:spec", (spec, results) => {
-        if (results && results.video) {
-          // Do we have failures for any retry attempts?
-          const failures = results.tests.some((test) =>
-            test.attempts.some((attempt) => attempt.state === "failed")
-          );
-          if (!failures) {
-            // delete the video if the spec passed and no tests retried
-            fs.unlinkSync(results.video);
-          }
-        }
       });
     },
   },
