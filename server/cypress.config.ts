7d46ac0f8eb5e00ae8779da94ddc1279f59a57d9
conversayShawn <shawn.harris3510@gmail.com>
2023-07-18 09:22:32 -0700

    v12: video=true all browsers, removing conditional video logic

--- server/cypress.config.ts
+++ server/cypress.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'cypress';
 import { createHero, deleteHero } from './cypress/support/data';
-import fs from 'fs'
 
 export default defineConfig({
   retries: {
@@ -17,21 +16,6 @@ export default defineConfig({
         createHero,
         deleteHero,
       });
-      on(
-        'after:spec',
-        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
-          if (results && results.video) {
-            // Do we have failures for any retry attempts?
-            const failures = results.tests.some((test) =>
-              test.attempts.some((attempt) => attempt.state === 'failed')
-            )
-            if (!failures) {
-              // delete the video if the spec passed and no tests retried
-              fs.unlinkSync(results.video)
-            }
-          }
-        }
-      )
     },
   },
 });
